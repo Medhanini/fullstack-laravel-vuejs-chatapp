f7c92274ecb030bfbd34b601040f6349a7acfa1f
Mohamed Hanini <mohamed.hanini95@gmail.com>
2021-11-17 21:28:08 +0100

    creating models relations [ The ChatMessage hasOne ChatRoom and OneUser] => done

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ChatMessage extends Model
 {
     use HasFactory;
+    public function room(){
+        return $this->hasOne('App\Models\ChatRomm','id','chat_room_id');
+    }
+    public function user(){
+        return $this->hasOne('App\Models\User','id','user_id');
+    }
 }
