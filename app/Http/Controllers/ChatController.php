6db1fdcea50339ab913403104d79bcec370c0a81
Mohamed Hanini <mohamed.hanini95@gmail.com>
2021-11-17 21:31:13 +0100

    creating Controller [ controllers\ChatController ] => done

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\ChatRoom;
+use App\Models\ChatMessage;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    //
+}
